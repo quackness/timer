a3ac25ee2f3d53f7c2f45cf3f2a096d4bffd636f
Karolina Redden <dubajkaro@gmail.com>
2022-02-24 20:02:53 +0000

    removing comments

--- timer1.js
+++ timer1.js
@@ -1,13 +1,11 @@
-const args = process.argv.slice(2);//slice is guarded, it knows how long the array is. If the array has less than 3 items it is not going to loop it, this takes care of the egde case with "no numbers provided"
+const args = process.argv.slice(2);
 for (let x of args) {
-  if (!isNaN(x)) {//if x is a number then execute the code inside, it filters out NaNs from entering the block
-    if (x >= 0) {//if x is greater or equal to 0, takes care of the edge case regarding negative numbers
-      x = 1000 * Number(x);//x is 1000 larger to make it in seconds
+  if (!isNaN(x)) {
+    if (x >= 0) {
+      x = 1000 * Number(x);
       setTimeout(() => {
-        process.stdout.write('Hello \n');//sound does not work for me so I used a string instead
+        process.stdout.write('Hello \n');
       }, x);
     }
   }
 }
-
-//isNaN https://www.w3schools.com/jsref/jsref_isnan.asp
